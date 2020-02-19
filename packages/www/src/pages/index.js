32d22a6ea8dfe2e67bb68aa82f0859e03fcb16f9
Anders Nygren <litemerafrukt@gmail.com>
2020-02-19 19:38:35 +0100

    Theming

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,7 +1,13 @@
 import React from "react"
+import { Container, Heading, Button, Flex } from "theme-ui"
 
 export default props => (
-  <div>
-    <h1>Hello shiny site!</h1>
-  </div>
+  <Container>
+    <Flex sx={{ flexDirection: "column", padding: 3 }}>
+      <Heading as="h1">JAMtodo</Heading>
+      <Button sx={{ marginBottom: 2 }} onClick={() => alert("Logging in!")}>
+        Log in
+      </Button>
+    </Flex>
+  </Container>
 )
