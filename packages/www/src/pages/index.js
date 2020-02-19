a55def8d7ba1da185d5cdcc848e6650187f49aa9
Anders Nygren <litemerafrukt@gmail.com>
2020-02-19 22:42:36 +0100

    Login
    Bonus: vscode eslinting

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,13 +1,20 @@
 import React from "react"
 import { Container, Heading, Button, Flex } from "theme-ui"
+import netlifyIdentity from "netlify-identity-widget"
 
-export default props => (
-  <Container>
-    <Flex sx={{ flexDirection: "column", padding: 3 }}>
-      <Heading as="h1">JAMtodo</Heading>
-      <Button sx={{ marginBottom: 2 }} onClick={() => alert("Logging in!")}>
-        Log in
-      </Button>
-    </Flex>
-  </Container>
-)
+export default props => {
+  React.useEffect(() => {
+    netlifyIdentity.init({})
+  })
+
+  return (
+    <Container>
+      <Flex sx={{ flexDirection: "column", padding: 3 }}>
+        <Heading as="h1">JAMtodo</Heading>
+        <Button sx={{ marginBottom: 2 }} onClick={() => netlifyIdentity.open()}>
+          Log in
+        </Button>
+      </Flex>
+    </Container>
+  )
+}
