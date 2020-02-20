6f8f7eba50cab994dc81aa589263875ad6a23a52
Anders Nygren <litemerafrukt@gmail.com>
2020-02-20 20:18:13 +0100

    Fix: Client-side routing and dashboard

--- packages/www/gatsby-config.js
+++ packages/www/gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     {
       resolve: "gatsby-plugin-create-client-paths",
-      options: { prefix: "/app/*" }
+      options: { prefixes: ["/app/*"] }
     }
   ]
 }
